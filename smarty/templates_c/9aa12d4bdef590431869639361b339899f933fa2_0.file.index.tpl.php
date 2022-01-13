b9b48fc7ace2b418908942eef812820d16ddd17e
Johannes <johannes.bluhm@outlook.com>
2022-01-13 14:13:22 +0100

    smarty

---
+++ smarty/templates_c/9aa12d4bdef590431869639361b339899f933fa2_0.file.index.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 4.0.3, created on 2022-01-13 13:12:44
+  from 'C:\Users\johan\Documents\Git\Bachelor-Thesis\smarty\templates\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.0.3',
+  'unifunc' => 'content_61e0254c84f9c6_48052025',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '9aa12d4bdef590431869639361b339899f933fa2' => 
+    array (
+      0 => 'C:\\Users\\johan\\Documents\\Git\\Bachelor-Thesis\\smarty\\templates\\index.tpl',
+      1 => 1642079544,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_61e0254c84f9c6_48052025 (Smarty_Internal_Template $_smarty_tpl) {
+?>Hello World!<?php }
+}

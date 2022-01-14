f4ae9bfe4de5736b0a3915a81eb9019280afa6b3
Johannes <johannes.bluhm@outlook.com>
2022-01-14 17:29:49 +0100

    struture

--- vanilla/index.php
+++ vanilla/index.php
@@ -1,10 +1,12 @@
 <?php
 
-require 'vendor/autoload.php';
+require 'require.php';
 
 echo "Hello World";
+$klaus = new User();
+print_r($klaus);
 
-$mysqli = SQL::init("localhost", "hsfl_ba");
+
 
 $test = SQL::select("SELECT * FROM test WHERE 1");
 print_r($test);
---
+++ vanilla/require.php
@@ -0,0 +1,21 @@
+<?php
+
+//Require composer
+require 'vendor/autoload.php';
+
+//Class autoloader
+spl_autoload_register(function ($className) {
+	//Replace slashes for unix os
+	$className = str_replace('\\', '/', $className);
+	$class = explode('/', $className);
+	//Capitalize each class name
+	array_walk($class, function (&$value) {
+		$value = ucfirst($value);
+	});
+	$className = implode('/', $class);
+
+	include_once __DIR__ . '/classes/' . $className . '.php';
+});
+
+//Init mysql
+$mysqli = SQL::init("localhost", "hsfl_ba");

79e5a48e5391775fa7d1898ba2582398309a7148
Johannes <johannes.bluhm@outlook.com>
2022-01-14 17:26:59 +0100

    sql

--- vanilla/index.php
+++ vanilla/index.php
@@ -1,2 +1,10 @@
 <?php
+
+require 'vendor/autoload.php';
+
 echo "Hello World";
+
+$mysqli = SQL::init("localhost", "hsfl_ba");
+
+$test = SQL::select("SELECT * FROM test WHERE 1");
+print_r($test);

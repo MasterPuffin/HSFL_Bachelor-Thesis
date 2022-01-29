e6bf4b421bb838e2c77d1cde21c4100e0fb7c6a6
Johannes <johannes.bluhm@outlook.com>
2022-01-29 17:40:06 +0100

    twig home

---
+++ twig2/index.php
@@ -0,0 +1,60 @@
+<?php
+
+
+require 'require.php';
+
+$nav = [
+	["Home", "home"],
+	["News", "news"],
+	["Mitarbeiter", "users"],
+	["Kontakt", "contact"],
+];
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader, [
+	'cache' => false,
+]);
+$twig->addExtension(new TwigExtension());
+
+
+//Explode Request to array and remove empty entries
+$request = array_diff(explode("/", $_SERVER['REQUEST_URI']), [""]);
+//Reindex array keys
+sort($request, SORT_NUMERIC);
+
+//Remove project name from request
+if (in_array($request[0], ["vanilla", "blade_laravel", "blade_standalone", "latte", "mustache", "pesto", "smarty", "twig2"])) {
+	unset($request[0]);
+	sort($request, SORT_NUMERIC);
+}
+
+//Redirect to home page when accessing /
+if (!isset($request[0])) {
+	redirect("home/");
+}
+
+switch ($request[0]) {
+	case 'home':
+		echo $twig->render('home.twig', ['nav' => $nav, 'news' => \News::getAll(4)]);
+		break;
+	case 'news':
+		if (isset($request[1])) {
+			//echo $blade->run("news", ['nav' => $nav, 'news' => new \News($request[1])]);
+		} else {
+			//echo $blade->run("newsGrid", ['nav' => $nav, 'news' => \News::getAll()]);
+		}
+		break;
+	case 'users':
+		if (isset($request[1])) {
+			//echo $blade->run("user", ['nav' => $nav, 'user' => new \User($request[1]), 'news' => $news = \News::getForUser($request[1])]);
+		} else {
+			//echo $blade->run("userGrid", ['nav' => $nav, 'departments' => \User::orderByDepartments(\User::getAll())]);
+		}
+		break;
+	case 'contact':
+		//echo $blade->run("contact", ['nav' => $nav, 'form' => $_POST]);
+		break;
+	default:
+		echo $twig->render('404.twig', ['nav' => $nav]);
+		break;
+}
---
+++ twig2/require.php
@@ -0,0 +1,29 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+const WEBROOT = "/twig2";
+const RESOURCESROOT = "";
+
+//Require composer
+require 'vendor/autoload.php';
+require 'functions.php';
+
+//Class autoloader
+spl_autoload_register(function ($className) {
+	//Replace slashes for unix os
+	$className = str_replace('\\', '/', $className);
+	$class = explode('/', $className);
+	//Capitalize each class name
+	array_walk($class, function (&$value) {
+		$value = ucfirst($value);
+	});
+	$className = implode('/', $class);
+
+	include_once __DIR__ . '/classes/' . $className . '.php';
+});
+
+//Init mysql
+$mysqli = SQL::init("localhost", "hsfl_ba");
